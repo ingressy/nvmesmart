60910e169939327b6da2896a1e72e4850a9763df
ingressy <j.czinzoll@ingressy.de>
2024-12-28 13:44:30 +0100

    v0.1.2 release

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="nvmesmart",             # Name des Pakets
-    version="0.1.1",                     # Versionsnummer
+    version="0.1.2",                     # Versionsnummer
     description="a easy cli and maybe gui smart tool for linux",  # Kurzbeschreibung
     author="ingressy",
     license="MIT",                       # Lizenz
     packages=find_packages(),            # Findet automatisch alle Python-Pakete
     entry_points={
         "console_scripts": [
-            "nvmesmart=nvmesmart.main:main",  # Befehl -> Funktion
+            "nvmesmart=main:main",  # Befehl -> Funktion
         ]
     },
     install_requires=[],                 # Abhängigkeiten (requirements.txt)
