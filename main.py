1894824043b6e3b2f832f18077de6e93b9ee9f32
ingressy <j.czinzoll@ingressy.de>
2024-12-28 12:55:12 +0100

    v0.1 release

---
+++ main.py
@@ -0,0 +1,18 @@
+import getpass, sys, os
+
+if getpass.getuser() == "root":
+    try:
+        if sys.argv[1] == "--table" or "-t":
+            try:
+                smartos = os.popen("sudo smartctl -a /dev/nvme0")
+                output = smartos.read().strip()
+                print(output)
+            except:
+                print("please specify a disk | e.g. /dev/sda")
+        else:
+            print("Please try again! ")
+    except:
+        print("nvmesmart v0.1 by ingressy")
+        print("Use -h or --help for Help")
+else:
+    print("please run as root!")
